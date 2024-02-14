9f425c7a61a2618f73afc262eaa1b3db099fa1cd
Vincent <nguyenhuuvui89@gmail.com>
2024-02-14 17:24:17 -0600

    form

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -1,4 +1,3 @@
-
 const initialFriends = [
   {
     id: 118836,
@@ -23,7 +22,7 @@ const initialFriends = [
 function App() {
   return (
     <div className="App">
-      <FriendList/>
+      <FriendList />
     </div>
   );
 }
@@ -34,26 +33,57 @@ function FriendList() {
       {initialFriends.map((e, i) => (
         <Friend friend={e} key={i} />
       ))}
-      <button type="button" className="addFriend btn btn-sm btn-warning ">Add friend</button>
+      <Button newClass="addFriend"> Add friend</Button>
+      <AddFriendForm />
+      <Button newClass="addFriend">Close</Button>
     </div>
   );
 }
 
-function Friend({friend}) {
+function Friend({ friend }) {
   return (
-    <div className='d-flex justify-content-between align-items-center m-2'>
+    <div className="d-flex justify-content-between align-items-center m-4">
       <img
         src={`${friend.image}`}
         className="rounded-circle"
-        style={{ width: "50px",  height: "50px"}}
+        style={{ width: "50px", height: "50px" }}
         alt="Avatar"
       />
-     <div  className='d-flex flex-column'>
+      <div className="d-flex flex-column">
         <span>{friend.name}</span>
-        <span className='green'>Expense note</span>
-     </div>
-     <button type="button" className="btn btn-sm btn-warning">Select</button>
+        <span className="green">Expense note</span>
+      </div>
+      <Button className =''>Select</Button>
     </div>
+  );
+}
+
+function AddFriendForm() {
+  return (
+    <div className='formClass p-3 mt-4'>
+      <Form formClass='formClass1'>🤼‍♀️ Friend name</Form>
+      <Form formClass='formClass1'>🏞️ Image URL</Form>
+      <div className ='addFr'>
+        <Button>Add</Button>
+      </div>
+    </div>
+  );
+}
+
+function Form({formClass, children}) {
+  return (
+    <div className={`${formClass}  m-2`}>
+      <label>{children}</label>
+      <input type="text" name="" id="" />
+    </div>
+  );
+}
+
+function Button({ children, newClass }) {
+  return (
+    <button type="button" className={`${newClass} btn btn-sm btn-warning`}>
+      {children}
+    </button>
   );
 }
 export default App;
